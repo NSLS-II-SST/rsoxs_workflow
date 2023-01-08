a01b7f7df1d165f7b3a44a15b18eb81611823059
Bijal Patel <54857110+BijalBPatel@users.noreply.github.com>
2023-01-08 00:19:17 -0500

    cleanup and more label of dryrun print text

--- rsoxs_scans/acquisition.py
+++ rsoxs_scans/acquisition.py
@@ -289,7 +289,8 @@ def dryrun_bar(bar, sort_by=["sample_num"], rev=[False], print_dry_run=True, gro
                 out["group"] = step[15]
                 out["slack_message_start"] = step[16]
                 out["slack_message_end"] = step[17]
-                statements.append(f'Step {j}:\n\t {out["description"]}')
+                statements.append(f"\tStep {j}\n")
+                statements.append(f"\t\t".join(s.strip() for s in out["description"].splitlines()))
 
                 if (out["action"]) == "error":
                     warnings.warn(f"WARNING: acquisition # {i} has a step with and error\n{out['description']}")
