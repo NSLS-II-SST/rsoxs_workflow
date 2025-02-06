358aca967598b8458c1153ebf5b6246ca5489972
Eliot Gann <egann@bnl.gov>
2025-02-06 15:04:41 -0500

    Moved spirals spot picking to a new file

---
+++ rsoxs_scans/spreadsheetManipulation.py
@@ -0,0 +1,13 @@
+
+
+## New spreadsheet 
+
+def loadSpreadsheet_Local():
+    ## TODO: copy over eliot's code piece by piece and make comments
+    ## TODO: Bar sheet should remain same but rename to samples maybe (see my partway done spreadsheet in rsoxs package)
+    ## TODO: redo acquisitions
+
+
+
+def sanitizeAcquisitions():
+    ##
