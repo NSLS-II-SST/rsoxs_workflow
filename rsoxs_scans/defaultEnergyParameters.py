970b3a123da2da026b2f756da025ce81d2cb35bb
PriyankaKetkarBNL <155195376+PriyankaKetkarBNL@users.noreply.github.com>
2025-02-11 11:48:15 -0500

    Added all of Eliot's RSoXS default energy parameters

--- rsoxs_scans/defaultEnergyParameters.py
+++ rsoxs_scans/defaultEnergyParameters.py
@@ -15,8 +15,12 @@ energyListParameters = {
     "nitrogen_RSoXS":  (380, 397, 0.3, 407, 0.1, 440, 0.3), ## Intended to recreate edge=(380, 397, 407, 440), ratios=(2, 0.2, 2), frames=112
     "oxygen_RSoXS":  (510, 525, 1.65, 540, 0.15, 560, 1.65), ## Intended to recreate edge=(510, 525, 540, 560), ratios=(2, 0.2, 2), frames=112
     "fluorine_RSoXS":  (670, 680, 1.65, 690, 0.15, 700, 0.5, 740, 1.65), ## Intended to recreate edge=(670, 680, 690, 700, 740), ratios=(2, 0.2, 0.6, 2), frames=112
+    "magnesium_RSoXS":  (1250, 1300, 4.2, 1330, 0.4, 1430, 4), ## Intended to recreate edge=(1250,1300,1330,1430), ratios=(2, 0.2, 2), frames=112
     "aluminum_RSoXS":  (1540, 1560, 2.2, 1580, 0.2, 1600, 2.2), ## Intended to recreate edge=(1540, 1560, 1580, 1600), ratios=(2, 0.2, 2), frames=112
+    "silicon_RSoXS":  (1820, 1840, 2.5, 1860, 0.2, 1910, 2.35), ## Intended to recreate edge=(1820,1840,1860,1910), ratios=(2, 0.2, 2), frames=112
     "sulfurL_RSoXS":  (150, 160, 1.25, 170, 0.1, 200, 1.25), ## Intended to recreate edge=(150, 160, 170, 200), ratios=(2, 0.2, 2), frames=112
+    "calciumL_RSoXS":  (320, 340, 2.2, 345, 0.45, 349, 0.2, 349.5, 0.05, 352.5, 0.1, 353, 0.05, 355, 0.5, 360, 0.45, 380, 0.2), ## Intended to recreate edge=(320, 340, 345, 349, 349.5, 352.5, 353, 355, 360, 380), ratios=(5, 1, 0.5, 0.1, 0.25, 0.1, 0.5, 1, 5), frames=112
+    "ironL_RSoXS":  (680, 700, 3.3, 730, 0.3, 780, 3.3), ## Intended to recreate edge=(680,700,730,780), ratios=(2, 0.2, 2), frames=112
     "zincL_RSoXS":  (1000, 1015, 2.5, 1035, 0.2, 1085, 2.35), ## Intended to recreate edge=(1000, 1015, 1035, 1085), ratios=(2, 0.2, 2), frames=112
 
     "carbon_NEXAFS_WSU":  (270, 278, 1, 283, 0.5, 291.5, 0.05, 300, 0.5, 330, 1, 350, 4), ## WSU NEXAFS for Brian Collins group
