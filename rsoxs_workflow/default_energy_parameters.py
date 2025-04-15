fbaece5f4bffb081abf137ded20ee370072f7fa6
PriyankaKetkarBNL <pketkar@bnl.gov>
2025-04-15 00:13:25 -0400

    Copied over default energy parameters from rsoxs codebase and made all key names lower case

---
+++ rsoxs_workflow/default_energy_parameters.py
@@ -0,0 +1,43 @@
+## This is intended to contain a revised set of default parameters that would be used after refactoring the codebase
+
+
+## These parameters would be fed into _make_gscan_points function in nbs_bl.plans.scan_base with the format (start, stop, step, stop, step, etc.)
+## In general, these are intended to recreate the energy lists from Eliot's old plan
+## TODO: ideally, I would like to import _make_gscan_points locally to test these scan parameters
+energy_list_parameters = {
+    "carbon_nexafs":  (250, 282, 1.45, 297, 0.3, 350, 1.45), ## This is intended to recreate edge=(250, 282, 297, 350), ratios=(5, 1, 5), frames=112 often used for carbon-edge NEXAFS
+    "nitrogen_nexafs":  (370, 397, 1, 407, 0.2, 440, 0.95), ## Intended to recreate edge=(370, 397, 407, 440), ratios=(5, 1, 5), frames=112
+    "oxygen_nexafs":  (500, 525, 1.1, 540, 0.2, 560, 1.1), ## Intended to recreate edge=(500, 525, 540, 560), ratios=(5, 1, 5), frames=112
+    "fluorine_nexafs":  (650, 680, 1.5, 700, 0.3, 740, 1.5), ## Intended to recreate edge=(650, 680, 700, 740), ratios=(5, 1, 5), frames=112
+    "magnesium_nexafs":  (1250, 1300, 2.6, 1330, 0.5, 1430, 2.7), ## Intended to recreate edge=(1250,1300,1330,1430), ratios=(5, 1, 5), frames=112
+    "aluminum_nexafs":  (1540, 1560, 1.6, 1580, 0.3, 1600, 1.6), ## Intended to recreate edge=(1500, 1560, 1580, 1600), ratios=(5, 1, 5), frames=112
+    "silicon_nexafs":  (1820, 1840, 1.5, 1860, 0.3, 1910, 1.5), ## Intended to recreate edge=(1820,1840,1860,1910), ratios=(5, 1, 5), frames=112
+    "sulfurL_nexafs":  (150, 160, 0.8, 170, 0.15, 200, 0.8), ## Intended to recreate edge=(150, 160, 170, 200), ratios=(5, 1, 5), frames=112
+    "calciumL_nexafs":  (320, 345, 0.9, 355, 0.15, 380, 0.9), ## Intended to recreate edge=(320, 345, 355, 380), ratios=(5, 1, 5), frames=112
+    "ironL_nexafs":  (680, 700, 2, 730, 0.35, 780, 2), ## Intended to recreate edge=(680,700,730,780), ratios=(5, 1, 5), frames=112
+    "zincL_nexafs":  (1000, 1015, 1.5, 1035, 0.3, 1085, 1.45), ## Intended to recreate edge=(1000, 1015, 1035, 1085), ratios=(5, 1, 5), frames=112
+    
+
+    "carbon_nexafs_wsu":  (270, 278, 1, 283, 0.5, 291.5, 0.05, 300, 0.5, 330, 1, 350, 4), ## WSU NEXAFS for Brian Collins group
+    "carbon_nexafs_reallyslow":  (250, 282, 0.3, 297, 0.07, 350, 0.3),
+    "carbon_nexafs_slow":  (250, 282, 0.3, 297, 0.1, 350, 0.3),
+    "carbon_nexafs_narrow": (282, 297, 0.3),
+    #"carbon_nexafs_reverse":  (350, 297, -1.45, 282, -0.3, 250, -1.45),
+
+
+    "carbon_rsoxs":  (250, 270, 5, 282, 1, 287, 0.1, 292, 0.2, 305, 1, 350, 5), ## Intended to recreate edge=(250, 270, 282, 287, 292, 305, 350), ratios=(5, 1, 0.1, 0.2, 1, 5), frames=112
+    "nitrogen_rsoxs":  (380, 397, 0.3, 407, 0.1, 440, 0.3), ## Intended to recreate edge=(380, 397, 407, 440), ratios=(2, 0.2, 2), frames=112
+    "oxygen_rsoxs":  (510, 525, 1.65, 540, 0.15, 560, 1.65), ## Intended to recreate edge=(510, 525, 540, 560), ratios=(2, 0.2, 2), frames=112
+    "fluorine_rsoxs":  (670, 680, 1.65, 690, 0.15, 700, 0.5, 740, 1.65), ## Intended to recreate edge=(670, 680, 690, 700, 740), ratios=(2, 0.2, 0.6, 2), frames=112
+    "magnesium_rsoxs":  (1250, 1300, 4.2, 1330, 0.4, 1430, 4), ## Intended to recreate edge=(1250,1300,1330,1430), ratios=(2, 0.2, 2), frames=112
+    "aluminum_rsoxs":  (1540, 1560, 2.2, 1580, 0.2, 1600, 2.2), ## Intended to recreate edge=(1540, 1560, 1580, 1600), ratios=(2, 0.2, 2), frames=112
+    "silicon_rsoxs":  (1820, 1840, 2.5, 1860, 0.2, 1910, 2.35), ## Intended to recreate edge=(1820,1840,1860,1910), ratios=(2, 0.2, 2), frames=112
+    "sulfurL_rsoxs":  (150, 160, 1.25, 170, 0.1, 200, 1.25), ## Intended to recreate edge=(150, 160, 170, 200), ratios=(2, 0.2, 2), frames=112
+    "calciumL_rsoxs":  (320, 340, 2.2, 345, 0.45, 349, 0.2, 349.5, 0.05, 352.5, 0.1, 353, 0.05, 355, 0.5, 360, 0.45, 380, 0.2), ## Intended to recreate edge=(320, 340, 345, 349, 349.5, 352.5, 353, 355, 360, 380), ratios=(5, 1, 0.5, 0.1, 0.25, 0.1, 0.5, 1, 5), frames=112
+    "ironL_rsoxs":  (680, 700, 3.3, 730, 0.3, 780, 3.3), ## Intended to recreate edge=(680,700,730,780), ratios=(2, 0.2, 2), frames=112
+    "zincL_rsoxs":  (1000, 1015, 2.5, 1035, 0.2, 1085, 2.35), ## Intended to recreate edge=(1000, 1015, 1035, 1085), ratios=(2, 0.2, 2), frames=112
+
+
+}
+
+#energies_eliot = get_energies(edge=[380, 397, 407, 440], ratios=[2, 0.2, 2], frames=112)
